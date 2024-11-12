fa5b2fa239fa36d01c1fc3fc9cbdd5b11e9462e8
m644933951 <36176868+m644933951@users.noreply.github.com>
2024-11-12 12:50:50 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -275,7 +275,8 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                        // 已放开注释
+			return new Response('Not found', { status: 404 });
                         url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
